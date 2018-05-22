4231aa2e92444341ca437925d8d73c1464b54687
vihugarcia <vihugarcia@gmail.com>
2018-05-22 00:47:49 -0300

    Remove unnecesary params in DataTable constructor

--- tinymvc/helpers/DataTable.php
+++ tinymvc/helpers/DataTable.php
@@ -3,26 +3,19 @@ namespace TinyMVC\helpers;
 
 class DataTable
 {
-    private $data;
     private $cols = [];
     private $key = '';
     private $controller;
-    private $page;
-    private $model;
 
-    public function __construct($cols, $key, $controller, $page, $model)
+    public function __construct($cols, $key, $controller)
     {
         $this->cols = $cols;
-        $this->data = $model->items($cols, ($page-1)*10);
         $this->key = $key;
         $this->controller = $controller;
-        $this->page = $page;
-        $this->model = $model;
     }
 
     public function render()
     {
-        $data = $this->data;
         $cols = $this->cols;
         $controller = $this->controller;
         $key = $this->key;
